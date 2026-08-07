ab5942a3ced3af45f2abb4fffca03dbc7c0e5a76
Gabriel-SAbreu <gabriel-abreu@procergs.rs.gov.br>
2026-08-07 14:58:09 -0300

    #1418443 - Alterado de @import para @font-face

--- src/components/BarraEstado/BarraEstadoStandalone.js
+++ src/components/BarraEstado/BarraEstadoStandalone.js
@@ -113,10 +113,24 @@ const DEFAULT_LINKS = [
   { href: 'https://www.rs.gov.br/guria', label: 'GurIA', srPrefix: 'Estado ', svgHtml: GURIA_SVG },
 ];
 
-const GOOGLE_FONTS_ROBOTO_URL = 'https://fonts.googleapis.com/css2?family=Roboto&display=swap';
-
 const CSS_TEXT = normalizeEscapedHtml(`
-@import url("${GOOGLE_FONTS_ROBOTO_URL}");
+/* @font-face declarado direto (sem @import) para funcionar de forma confiável dentro do Shadow DOM */
+@font-face {
+  font-family: 'Roboto';
+  font-style: normal;
+  font-weight: 400;
+  font-display: swap;
+  src: url(https://fonts.gstatic.com/s/roboto/v51/KFOMCnqEu92Fr1ME7kSn66aGLdTylUAMQXC89YmC2DPNWubEbVmaiArmlw.woff2) format('woff2');
+  unicode-range: U+0100-02BA, U+02BD-02C5, U+02C7-02CC, U+02CE-02D7, U+02DD-02FF, U+0304, U+0308, U+0329, U+1D00-1DBF, U+1E00-1E9F, U+1EF2-1EFF, U+2020, U+20A0-20AB, U+20AD-20C0, U+2113, U+2C60-2C7F, U+A720-A7FF;
+}
+@font-face {
+  font-family: 'Roboto';
+  font-style: normal;
+  font-weight: 400;
+  font-display: swap;
+  src: url(https://fonts.gstatic.com/s/roboto/v51/KFOMCnqEu92Fr1ME7kSn66aGLdTylUAMQXC89YmC2DPNWubEbVmUiAo.woff2) format('woff2');
+  unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+0304, U+0308, U+0329, U+2000-206F, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
+}
 
 :host {
   --barra-estado-height: 32px;
